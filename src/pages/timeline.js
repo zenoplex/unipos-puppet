ab8b02f0f5ea49e2b6b2a0b458658b02a8dbcafb
Gen Ichino <zenoplex@gmail.com>
2017-09-14 18:19:11 +0900

    refactor: adjust timing

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -13,10 +13,12 @@ const submit: SubmitOptions => Page => Promise<Page>
   const hash = hashtag ? `#${hashtag}` : '';
   await page.waitForSelector('#post-form-input');
   await page.focus('#post-form-input');
+  // needs deplay so textarea dropdown won't repeatedly open
   await page.type(`@${username} +${point} ${message} ${hash}`, { delay: 50 });
+  await page.press('Escape');
   await page.click('.postBtn');
   await page.waitForSelector('.postBtn[disabled]');
-  await page.waitFor(5000);
+  await page.waitFor(3000);
   return page;
 };
 
